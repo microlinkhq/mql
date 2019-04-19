69efe5d701795ff523ad95da6e47c03843cd0e4b
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-19 13:05:39 +0200

    test: ensure query parameters of the url are encoded

--- test/opts.js
+++ test/opts.js
@@ -9,7 +9,9 @@ import mqlNode from '../src/node'
   { constructor: mqlBrowser, target: 'browser' }
 ].forEach(({ constructor: mql, target }) => {
   test(`${target} » url`, async t => {
-    const { status, data, response } = await mql('https://kikobeats.com')
+    const { status, data, response } = await mql(
+      'https://kikobeats.com?ref=mql'
+    )
     t.snapshot(status)
     t.snapshot(data)
     t.snapshot(response.url)
@@ -19,9 +21,9 @@ import mqlNode from '../src/node'
     test('node » cache support', async t => {
       const cache = new Map()
       let data
-      data = await mql('https://kikobeats.com', { cache })
+      data = await mql('https://kikobeats.com?ref=mql', { cache })
       t.is(data.response.fromCache, false)
-      data = await mql('https://kikobeats.com', { cache })
+      data = await mql('https://kikobeats.com?ref=mql', { cache })
       t.is(data.response.fromCache, true)
     })
   }
