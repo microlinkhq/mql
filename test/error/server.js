5f62d0fe9292767fc36735607013a177411bde3b
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-08-23 13:32:48 +0200

    fix: linter

--- test/error/server.js
+++ test/error/server.js
@@ -3,7 +3,7 @@
 import test from 'ava'
 import mqlBrowser from '../../src/browser'
 import mqlNode from '../../src/node'
-  ;[
+;[
   { constructor: mqlNode, target: 'node' },
   { constructor: mqlBrowser, target: 'browser' }
 ].forEach(({ constructor: mql, target }) => {
