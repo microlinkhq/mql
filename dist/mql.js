0355cf059769d3b1ba7c214caed6f5c58d82783a
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-08-24 11:56:27 +0200

    build: generate bundle

--- dist/mql.js
+++ dist/mql.js
@@ -2358,11 +2358,6 @@
 	  PRO: 'https://pro.microlink.io'
 	};
 
-	const ERRORS_CODE = {
-	  INVALID_URL: 'ENOVALIDURL',
-	  FAILED: 'EFAILED'
-	};
-
 	function factory ({
 	  VERSION,
 	  MicrolinkError,
@@ -2373,10 +2368,14 @@
 	}) {
 	  const assertUrl = (url = '') => {
 	    if (!isUrlHttp(url)) {
+	      const message = `The \`url\` as \`${url}\` is not valid. Ensure it has protocol (http or https) and hostname.`;
 	      throw new MicrolinkError({
 	        url,
-	        code: ERRORS_CODE.INVALID_URL,
-	        message: `The URL \`${url}\` is not valid. Ensure it has protocol (http or https) and hostname.`
+	        data: { url: message },
+	        status: 'fail',
+	        code: 'EINVALURLCLIENT',
+	        message,
+	        more: 'https://microlink.io/docs/api/api-parameters/url'
 	      })
 	    }
 	  };
@@ -2389,28 +2388,44 @@
 	    )
 	  };
 
-	  const fetchFromApi = async (url, opts) => {
+	  const fetchFromApi = async (url, apiUrl, opts) => {
 	    try {
-	      const response = await got(url, opts);
+	      const response = await got(apiUrl, opts);
 	      const { body } = response;
 	      return { ...body, response }
 	    } catch (err) {
+	      if (err.name === 'TimeoutError') {
+	        const message = `The \`url\` as \`${url}\` reached timeout after ${
+          opts.retry.maxRetryAfter
+        }ms.`;
+	        throw new MicrolinkError({
+	          url,
+	          data: { url: message },
+	          status: 'fail',
+	          code: 'ETIMEOUTCLIENT',
+	          message,
+	          more: 'https://microlink.io/docs/api/api-parameters/url'
+	        })
+	      }
 	      const body = err.body
 	        ? typeof err.body === 'string' || Buffer.isBuffer(err.body)
 	          ? JSON.parse(err.body)
 	          : err.body
 	        : { message: err.message, status: 'fail' };
+	      const message = body.data
+	        ? body.data[Object.keys(body.data)[0]]
+	        : body.message;
 	      const { statusCode = 500 } = err;
 	      throw MicrolinkError({
 	        ...body,
+	        message,
 	        url,
-	        statusCode,
-	        code: ERRORS_CODE.FAILED
+	        statusCode
 	      })
 	    }
 	  };
 
-	  const apiUrl = (url, { rules, apiKey, endpoint, ...opts } = {}) => {
+	  const getApiUrl = (url, { rules, apiKey, endpoint, ...opts } = {}) => {
 	    const isPro = !!apiKey;
 	    const apiEndpoint = endpoint || ENDPOINT[isPro ? 'PRO' : 'FREE'];
 
@@ -2425,19 +2440,20 @@
 	  };
 
 	  const mql = async (
-	    targetUrl,
+	    url,
 	    {
 	      encoding = 'utf8',
-	      cache = null,
-	      retry = 3,
-	      timeout = 25000,
+	      cache = false,
+	      retry = 2,
+	      timeout = 30000,
 	      json = true,
 	      ...opts
 	    } = {}
 	  ) => {
-	    assertUrl(targetUrl);
-	    const [url, { headers }] = apiUrl(targetUrl, opts);
-	    return fetchFromApi(url, {
+	    assertUrl(url);
+	    const [apiUrl, { headers }] = getApiUrl(url, opts);
+
+	    return fetchFromApi(url, apiUrl, {
 	      encoding,
 	      cache,
 	      retry,
@@ -2448,7 +2464,7 @@
 	  };
 
 	  mql.MicrolinkError = MicrolinkError;
-	  mql.apiUrl = apiUrl;
+	  mql.apiUrl = getApiUrl;
 	  mql.mapRules = mapRules;
 	  mql.version = VERSION;
 	  mql.stream = got.stream;
@@ -2475,9 +2491,11 @@
 	    const { headers, status: statusCode, statusText: statusMessage } = response;
 	    return { url: response.url, body, headers, statusCode, statusMessage }
 	  } catch (err) {
-	    err.body = err.response ? await err.response.json() : '';
-	    err.statusCode = err.response.status;
-	    err.headers = err.response.headers;
+	    if (err.response) {
+	      err.body = await err.response.json();
+	      err.statusCode = err.response.status;
+	      err.headers = err.response.headers;
+	    }
 	    throw err
 	  }
 	};
@@ -2488,7 +2506,7 @@
 	  stringify,
 	  got,
 	  flatten: flat,
-	  VERSION: '0.3.16'
+	  VERSION: '0.4.0'
 	});
 
 	var browser_1 = browser;
