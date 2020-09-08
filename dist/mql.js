5a1373b04ca1bf7373027c38711825e2e872ff6c
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-09-08 22:19:35 +0200

    build(no-release): generate bundle

--- dist/mql.js
+++ dist/mql.js
@@ -614,6 +614,33 @@
 		decode: decode
 	});
 
+	function iter(output, sep, val, key) {
+		var k, pfx = key ? (key + sep) : key;
+
+		if (val == null) ; else if (typeof val != 'object') {
+			output[key] = val;
+		} else if (Array.isArray(val)) {
+			for (k=0; k < val.length; k++) {
+				iter(output, sep, val[k], pfx + k);
+			}
+		} else {
+			for (k in val) {
+				iter(output, sep, val[k], pfx + k);
+			}
+		}
+	}
+
+	function flattie(input, sep) {
+		var output = {};
+		if (typeof input == 'object') iter(output, sep || '.', input, '');
+		return output;
+	}
+
+	var dist = /*#__PURE__*/Object.freeze({
+		__proto__: null,
+		flattie: flattie
+	});
+
 	var _rollupPluginShim1 = str => str;
 
 	var _rollupPluginShim1$1 = /*#__PURE__*/Object.freeze({
@@ -793,165 +820,6 @@
 	lib.reference = reference;
 	lib.uri = uri;
 
-	var flat = flatten;
-	flatten.flatten = flatten;
-	flatten.unflatten = unflatten;
-
-	function isBuffer (obj) {
-	  return obj &&
-	    obj.constructor &&
-	    (typeof obj.constructor.isBuffer === 'function') &&
-	    obj.constructor.isBuffer(obj)
-	}
-
-	function keyIdentity (key) {
-	  return key
-	}
-
-	function flatten (target, opts) {
-	  opts = opts || {};
-
-	  const delimiter = opts.delimiter || '.';
-	  const maxDepth = opts.maxDepth;
-	  const transformKey = opts.transformKey || keyIdentity;
-	  const output = {};
-
-	  function step (object, prev, currentDepth) {
-	    currentDepth = currentDepth || 1;
-	    Object.keys(object).forEach(function (key) {
-	      const value = object[key];
-	      const isarray = opts.safe && Array.isArray(value);
-	      const type = Object.prototype.toString.call(value);
-	      const isbuffer = isBuffer(value);
-	      const isobject = (
-	        type === '[object Object]' ||
-	        type === '[object Array]'
-	      );
-
-	      const newKey = prev
-	        ? prev + delimiter + transformKey(key)
-	        : transformKey(key);
-
-	      if (!isarray && !isbuffer && isobject && Object.keys(value).length &&
-	        (!opts.maxDepth || currentDepth < maxDepth)) {
-	        return step(value, newKey, currentDepth + 1)
-	      }
-
-	      output[newKey] = value;
-	    });
-	  }
-
-	  step(target);
-
-	  return output
-	}
-
-	function unflatten (target, opts) {
-	  opts = opts || {};
-
-	  const delimiter = opts.delimiter || '.';
-	  const overwrite = opts.overwrite || false;
-	  const transformKey = opts.transformKey || keyIdentity;
-	  const result = {};
-
-	  const isbuffer = isBuffer(target);
-	  if (isbuffer || Object.prototype.toString.call(target) !== '[object Object]') {
-	    return target
-	  }
-
-	  // safely ensure that the key is
-	  // an integer.
-	  function getkey (key) {
-	    const parsedKey = Number(key);
-
-	    return (
-	      isNaN(parsedKey) ||
-	      key.indexOf('.') !== -1 ||
-	      opts.object
-	    ) ? key
-	      : parsedKey
-	  }
-
-	  function addKeys (keyPrefix, recipient, target) {
-	    return Object.keys(target).reduce(function (result, key) {
-	      result[keyPrefix + delimiter + key] = target[key];
-
-	      return result
-	    }, recipient)
-	  }
-
-	  function isEmpty (val) {
-	    const type = Object.prototype.toString.call(val);
-	    const isArray = type === '[object Array]';
-	    const isObject = type === '[object Object]';
-
-	    if (!val) {
-	      return true
-	    } else if (isArray) {
-	      return !val.length
-	    } else if (isObject) {
-	      return !Object.keys(val).length
-	    }
-	  }
-
-	  target = Object.keys(target).reduce(function (result, key) {
-	    const type = Object.prototype.toString.call(target[key]);
-	    const isObject = (type === '[object Object]' || type === '[object Array]');
-	    if (!isObject || isEmpty(target[key])) {
-	      result[key] = target[key];
-	      return result
-	    } else {
-	      return addKeys(
-	        key,
-	        result,
-	        flatten(target[key], opts)
-	      )
-	    }
-	  }, {});
-
-	  Object.keys(target).forEach(function (key) {
-	    const split = key.split(delimiter).map(transformKey);
-	    let key1 = getkey(split.shift());
-	    let key2 = getkey(split[0]);
-	    let recipient = result;
-
-	    while (key2 !== undefined) {
-	      if (key1 === '__proto__') {
-	        return
-	      }
-
-	      const type = Object.prototype.toString.call(recipient[key1]);
-	      const isobject = (
-	        type === '[object Object]' ||
-	        type === '[object Array]'
-	      );
-
-	      // do not write over falsey, non-undefined values if overwrite is false
-	      if (!overwrite && !isobject && typeof recipient[key1] !== 'undefined') {
-	        return
-	      }
-
-	      if ((overwrite && !isobject) || (!overwrite && recipient[key1] == null)) {
-	        recipient[key1] = (
-	          typeof key2 === 'number' &&
-	          !opts.object ? [] : {}
-	        );
-	      }
-
-	      recipient = recipient[key1];
-	      if (split.length > 0) {
-	        key1 = getkey(split.shift());
-	        key2 = getkey(split[0]);
-	      }
-	    }
-
-	    // unflatten again for 'messy objects'
-	    recipient[key1] = unflatten(target[key], opts);
-	  });
-
-	  return result
-	}
-
 	const ENDPOINT = {
 	  FREE: 'https://api.microlink.io',
 	  PRO: 'https://pro.microlink.io'
@@ -1065,10 +933,12 @@
 
 	var factory_1 = factory;
 
+	var require$$2 = getCjsExportFromNamespace(dist);
+
 	const ky = kyUmd.default || kyUmd;
 
 	const { encode: stringify } = qss_m;
-
+	const { flattie: flatten } = require$$2;
 
 
 
@@ -1104,8 +974,8 @@
 	  isUrlHttp: lightweight,
 	  stringify,
 	  got,
-	  flatten: flat,
-	  VERSION: '0.7.6'
+	  flatten,
+	  VERSION: '0.7.7'
 	});
 
 	return browser;
