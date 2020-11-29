9e0ae8245a3950ccae00193de39a8c993ad97af4
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-11-29 14:10:20 +0100

    build: use lightweight version

--- src/node.js
+++ src/node.js
@@ -1,6 +1,6 @@
 module.exports = require('./factory')({
   MicrolinkError: require('whoops')('MicrolinkError'),
-  isUrlHttp: require('is-url-http'),
+  isUrlHttp: require('is-url-http/lightweight'),
   stringify: require('querystring').stringify,
   got: require('got'),
   flatten: require('flattie').flattie,
