2e77319df0864b2962e53113105c4eeb0ed07cf8
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-10-02 00:13:10 +0200

    feat: add EFATALCLIENT error code

--- test/error/client.js
+++ test/error/client.js
@@ -17,4 +17,17 @@ clients.forEach(({ constructor: mql, target }) => {
     t.true(!!error.message)
     t.true(!!error.description)
   })
+
+  test(`${target} » EFATALCLIENT`, async t => {
+    const error = await t.throwsAsync(mql('https://example.com', { endpoint: 'https://notexist.dev' }), { instanceOf: mql.MicrolinkError })
+
+    t.true(error.url === 'https://notexist.dev?url=https%3A%2F%2Fexample.com')
+    t.true(error.code === 'EFATALCLIENT')
+    t.true(error.status === 'error')
+    t.true(error.more === 'https://microlink.io/efatalclient')
+    t.true(error.statusCode === undefined)
+    t.true(!!error.data)
+    t.true(!!error.message)
+    t.true(!!error.description)
+  })
 })
