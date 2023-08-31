d945c8456aa2768e3ab4a0aedb8124b2e5a8e55d
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-08-31 12:00:57 +0200

    test: add linter for types

---
+++ test/index.test-d.ts
@@ -0,0 +1,94 @@
+// import { expectType } from 'tsd'
+
+import mql from '../index'
+
+/** mql */
+mql('https://example.com', {
+  endpoint: 'https://pro.microlink.io',
+  apiKey: '123',
+  retry: 2,
+  cache: new Map()
+})
+
+/**
+ * data
+ */
+mql('https://example.com', {
+  data: {
+    version: {
+      evaluate: 'window.next.version',
+      type: 'string'
+    }
+  }
+})
+
+mql('https://github.com/microlinkhq', {
+  data: {
+    stats: {
+      selector: '.application-main',
+      attr: {
+        followers: {
+          selector: '.js-profile-editable-area a[href*="tab=followers"] span',
+          type: 'number'
+        },
+        following: {
+          selector: '.js-profile-editable-area a[href*="tab=following"] span',
+          type: 'number'
+        },
+        stars: {
+          selector:
+            '.js-responsive-underlinenav a[data-tab-item="stars"] span',
+          type: 'number'
+        }
+      }
+    }
+  }
+})
+
+/**
+ * meta
+ */
+mql('https://example.com')
+mql('https://example.com', { meta: true })
+mql('https://example.com', { meta: { logo: { square: true } } })
+
+/**
+ * pdf
+ */
+mql('https://example.com', { pdf: true })
+mql('https://example.com', {
+  pdf: {
+    format: 'A4',
+    width: '480px',
+    margin: {
+      top: '4mm',
+      bottom: '4mm',
+      left: '4mm',
+      right: '4mm'
+    }
+  }
+})
+
+/**
+ * screenshot
+ */
+mql('https://example.com', { screenshot: true })
+mql('https://example.com', {
+  screenshot: {
+    codeScheme: 'atom-dark',
+    type: 'png',
+    overlay: {
+      background: '#000',
+      browser: 'light'
+    }
+  }
+})
+
+/** others */
+mql('https://example.com', { click: ['div'] })
+mql('https://example.com', {
+  adblock: true,
+  animations: false,
+  audio: true,
+  video: true
+})
